ca2b99151c7670acc19581494b5c8c701e466229
Sven <christsven@gmx.de>
2025-09-04 21:52:22 +0200

    Updating .dotfiles...

--- .scripts/copyDotfiles.sh
+++ .scripts/copyDotfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-REPO_PATH="~/src/.dotfiles/"
+REPO_PATH="~/src/.dotfiles"
 
 # copy files to repository
-sudo cp -r ~/.config/nvim $REPO_PATH
-sudo cp -r ~/.config/i3 $REPO_PATH
-sudo cp -r ~/.config/polybar $REPO_PATH
+cp -r ~/.config/nvim $REPO_PATH
+cp -r ~/.config/i3 $REPO_PATH
+cp -r ~/.config/polybar $REPO_PATH
 
 # generate commit
 git add .
